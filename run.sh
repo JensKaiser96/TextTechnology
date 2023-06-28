182227070c61618146fae7d093502d0e6ccf994d
Jens Kaiser <jens.kaiser@ims.uni-stuttgart.de>
2023-06-28 18:40:58 +0200

    changed example path to data/ dir

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Usage: $ run.sh <OutputFileName>
-# Example: $ run.sh gute_frage.json
+# Example: $ run.sh data/gute_frage.json
 cd main/
 python -m pipenv run python -m scrapy crawl GuteFrage -o $1
 cat $1 | jq >> "${1}_"
